b64eeb63f993d25aefda62fc8b41d91332fb6d66
Swapnil Nade <141449289+snax-07@users.noreply.github.com>
2023-12-27 23:16:05 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+/*====== This function are linked to the main/Landing page of this organisation======*/
+
+/*===== Here are the basic function provided to the main/ Landing page of our page Following function is used to locate the feature html file which is located in feature folder  =====*/
+
+
+function fea(){
+  window.location.href = "feature/fea.html";
+}
