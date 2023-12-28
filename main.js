27314c33a8d842f89c727561aa8be33064f2c861
Swapnil Nade <141449289+snax-07@users.noreply.github.com>
2023-12-28 07:03:53 +0530

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-/*====== This function are linked to the main/Landing page of this organisation======*/
-
-/*===== Here are the basic function provided to the main/ Landing page of our page Following function is used to locate the feature html file which is located in feature folder  =====*/
-
-
-function fea(){
-  window.location.href = "feature/fea.html";
-}
